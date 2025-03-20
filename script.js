70315e0a35de1b25f5a881877a5f4f62122a53a4
Samuel Calixto <108810329+Samuelcalix@users.noreply.github.com>
2025-03-20 17:50:28 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,47 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const imagens1 = [
+    "/assets/helo1.jpg",
+    "/assets/helo2.jpg",
+    "/assets/helo3.jpg",
+    "/assets/helo4.jpg",
+    "/assets/helo5.jpg",
+    "/assets/helo6.jpg",
+    "/assets/helo7.jpg",
+  ];
+
+  const imagens2 = [
+    "/assets/helo2.jpg",
+    "/assets/helo3.jpg",
+    "/assets/helo4.jpg",
+    "/assets/helo5.jpg",
+    "/assets/helo6.jpg",
+    "/assets/helo7.jpg",
+    "/assets/helo1.jpg",
+  ];
+
+  let index1 = 0;
+  let index2 = 0;
+
+  function trocarImagem() {
+    const imgPolaroid1 = document.getElementById("img-polaroid1");
+    const imgPolaroid2 = document.getElementById("img-polaroid2");
+
+    if (imgPolaroid1 && imgPolaroid2) {
+      imgPolaroid1.classList.add("fade-out");
+      imgPolaroid2.classList.add("fade-out");
+
+      setTimeout(() => {
+        index1 = (index1 + 1) % imagens1.length; // Alterna entre 0 e 6 (7 imagens)
+        index2 = (index2 + 1) % imagens2.length; // Alterna entre 0 e 6 (7 imagens)
+
+        imgPolaroid1.src = imagens1[index1];
+        imgPolaroid2.src = imagens2[index2];
+
+        imgPolaroid1.classList.remove("fade-out");
+        imgPolaroid2.classList.remove("fade-out");
+      }, 1000);
+    }
+  }
+
+  setInterval(trocarImagem, 3000);
+});
