8b5f5cf10a2ec0bc88e9053c7730c2fef7845022
Samuel Calixto <108810329+Samuelcalix@users.noreply.github.com>
2025-03-21 06:35:35 -0300

    Update script.js

--- script.js
+++ script.js
@@ -11,11 +11,11 @@ document.addEventListener("DOMContentLoaded", () => {
 
   const imagens2 = [
     "/niver-helo/helo2.jpg",
-    "/niver-helo/helo3.jpg",
     "/niver-helo/helo4.jpg",
-    "/niver-helo/helo5.jpg",
     "/niver-helo/helo6.jpg",
     "/niver-helo/helo7.jpg",
+    "/niver-helo/helo3.jpg",
+    "/niver-helo/helo5.jpg",
     "/niver-helo/helo1.jpg",
   ];
 
